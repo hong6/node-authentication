6c6976a71d02cb682743050be4392aa188f785ad
xiaohong6 <xiaohongliu2006@gmail.com>
2013-12-17 20:52:20 -0500

    init v1.0.0

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url': 'mongodb://hong6:hellohello@mongo.onmodulus.net:27017/xE9jased' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
