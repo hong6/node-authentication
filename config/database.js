d11e8cf86125ad732555cd75ee5e82d1b99df000
hong6 <ccliu68@gmail.com>
2013-12-20 14:59:05 -0500

    changed DB to local

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url': 'mongodb://hong6:hellohello@mongo.onmodulus.net:27017/xE9jased'
-
+    //'url': 'mongodb://hong6:hellohello@mongo.onmodulus.net:27017/xE9jased'
+    'url': 'mongodb://localhost:27017/'
 };
